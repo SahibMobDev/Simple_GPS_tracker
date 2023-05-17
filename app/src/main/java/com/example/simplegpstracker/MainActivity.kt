17eb8636328283cb0a9dfe6228031a7420bc15db
SahibMobDev <sahibmobdev@gmail.com>
2023-05-17 18:12:39 +0400

    Create bottom navigation menu

--- app/src/main/java/com/example/simplegpstracker/MainActivity.kt
+++ app/src/main/java/com/example/simplegpstracker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simplegpstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.simplegpstracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        onBottomNavClick()
+    }
+
+    private fun onBottomNavClick() {
+        binding.bNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.id_home -> {
+                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                }
+                R.id.id_tracks -> {
+                    Toast.makeText(this, "Tracks", Toast.LENGTH_SHORT).show()
+                }
+                R.id.id_settings -> {
+                    Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 }
