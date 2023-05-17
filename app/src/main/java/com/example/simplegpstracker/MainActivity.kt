7501e4a8f0397c5d87ac97ea088014c9f3012380
SahibMobDev <sahibmobdev@gmail.com>
2023-05-17 16:39:25 +0400

    ViewBinding etc

--- app/src/main/java/com/example/simplegpstracker/MainActivity.kt
+++ app/src/main/java/com/example/simplegpstracker/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.simplegpstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simplegpstracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
