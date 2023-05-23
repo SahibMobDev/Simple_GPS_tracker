8e04e60d2d1e16e8fb4b086b6dd8848b3fe08409
SahibMobDev <sahibmobdev@gmail.com>
2023-05-23 05:07:48 +0400

    Create fragment for settings #2

--- app/src/main/java/com/example/simplegpstracker/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/simplegpstracker/fragments/SettingsFragment.kt
@@ -1,12 +1,34 @@
 package com.example.simplegpstracker.fragments
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.simplegpstracker.R
+import com.example.simplegpstracker.utils.showToast
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    lateinit var timePref: Preference
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.main_preference, rootKey)
+        init()
+    }
+
+    private fun init() {
+        timePref = findPreference("update_time_key")!!
+        val changeListener = onChangedListener()
+        timePref.onPreferenceChangeListener = changeListener
+    }
+
+    private fun onChangedListener() : Preference.OnPreferenceChangeListener {
+        return Preference.OnPreferenceChangeListener {
+            pref, value ->
+            val nameArray = resources.getStringArray(R.array.loc_time_update_name)
+            val valueArray = resources.getStringArray(R.array.loc_time_update_value)
+            val title = pref.title.toString().substringBefore(":")
+            pref.title = "$title: ${nameArray[valueArray.indexOf(value)]}"
+
+            true
+        }
     }
 
 }
