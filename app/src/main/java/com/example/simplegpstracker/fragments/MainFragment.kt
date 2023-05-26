99d74efb6a6e9c8691e8b50bd39e48f6635e6be6
SahibMobDev <sahibmobdev@gmail.com>
2023-05-26 22:38:42 +0400

    Connect and setup Open Street Map

--- app/src/main/java/com/example/simplegpstracker/fragments/MainFragment.kt
+++ app/src/main/java/com/example/simplegpstracker/fragments/MainFragment.kt
@@ -1,11 +1,15 @@
 package com.example.simplegpstracker.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.simplegpstracker.databinding.FragmentMainBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.library.BuildConfig
 
 class MainFragment : Fragment() {
     lateinit var binding: FragmentMainBinding
@@ -14,8 +18,17 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        settingsOsm()
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun settingsOsm() {
+        Configuration.getInstance().load(
+            activity as AppCompatActivity,
+            activity?.getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
+        )
+        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
     }
 
     companion object {
